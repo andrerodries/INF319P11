803c4a498a8086fcb6e6fc4dac0fafbb7a46e96d
andrerodries <39647031+andrerodries@users.noreply.github.com>
2021-09-22 22:56:04 -0400

    Add files via upload

---
+++ EJERCICIO3/moda.R
@@ -0,0 +1,5 @@
+#DATOS 
+x<-c(3,4,5,2,3,4,1,2,3,8,5,6,7)
+#LA MODA ES EL DATO QUE MAS SE REPITE
+as.numeric(names(which.max(table(x))))
+
---
+++ EJERCICIO5/EJERCICIO5/Program.fs
@@ -0,0 +1,17 @@
+﻿// Más información acerca de F# en http://fsharp.org
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+
+open System 
+
+let listam a b =[for i in 1..a -> b]
+let a = 6
+let b = 7
+let lista = listam a b
+printfn "dado 2 numeros" 
+printfn "%d " a
+printfn "%d " b
+printfn "la multiplicacion es" 
+let suma = List.sum lista
+printfn "%d " suma
+
+Console.Read()
