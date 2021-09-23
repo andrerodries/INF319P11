73f37b16a786b58573280d12242016f20eb678df
andrerodries <39647031+andrerodries@users.noreply.github.com>
2021-09-22 22:38:40 -0400

    Add files via upload

---
+++ EJERCICIO3/moda.R
@@ -0,0 +1,3 @@
+x<-c(3,4,5,2,3,4,1,2,3,8,5,6,7)
+as.numeric(names(which.max(table(x))))
+
---
+++ EJERCICIO4/EJERCICIO4/Program.fs
@@ -0,0 +1,23 @@
+﻿// Más información acerca de F# en http://fsharp.org
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+
+open System
+let lista = [ 1; 2; 3; 4; 5; 6; 7; 8; 9; 10 ]
+
+printfn "NUMEROS DE LA LISTA"
+for i in lista do
+    printf "%i " i 
+printfn "" 
+let par i = i%2=0
+let impar i = i%2<>0
+printfn "pares"
+for i in lista do
+    let r = par i
+    if r then printf "%i " i  
+printfn ""       
+printfn "impares"
+for i in lista do
+    let r = impar i
+    if r then printf "%i " i 
+        
+Console.Read()
